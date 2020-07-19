ab36b8732d30a6ef8eb8edcda975e1b00174e545
Edson Isaac <65103163+Edssaac@users.noreply.github.com>
2020-07-18 23:31:27 -0300

    add botao

--- game.js
+++ game.js
@@ -13,10 +13,10 @@ function atualizaMostrador() {
 
     if (playTime == player1) {
         var player = document.querySelectorAll("div#head img")[0];
-        player.setAttribute("src", "x.png")
+        player.setAttribute("src", "assets/x.png")
     } else {
         var player = document.querySelectorAll("div#head img")[0];
-        player.setAttribute("src", "bolinha.svg")
+        player.setAttribute("src", "assets/bolinha.svg")
     }
 } 
 
@@ -32,11 +32,11 @@ function inicializarEspacos() {
             if (this.getElementsByTagName("img").length == 0) {
                 
                 if (playTime == player1) {
-                    this.innerHTML = "<img src='x.png' height='55'>";
+                    this.innerHTML = "<img src='assets/x.png' height='55'>";
                     this.setAttribute("jogada", player1);
                     playTime = player2;
                 } else {
-                    this.innerHTML = "<img src='bolinha.svg' height='55'>";
+                    this.innerHTML = "<img src='assets/bolinha.svg' height='55'>";
                     this.setAttribute("jogada", player2);
                     playTime = player1;
                 }
@@ -87,4 +87,23 @@ async function verificarVencedor() {
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function restart() {
+    playTime = player1;
+
+    var player = document.querySelectorAll("div#head img")[0];
+    player.setAttribute("src", "assets/x.png");
+
+    
+
+
+
+
+
+    
+
+
+
+    
 }
